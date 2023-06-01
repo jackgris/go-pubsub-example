6e3da7dc666260a53f8400c230c6c6440d808c82
Gabriel Pozo <jackgris2@gmail.com>
2023-06-01 00:21:21 -0300

    add: the server now use the pub/sub service to send the messages
    Now all the clients will receive the messages from all. To simulate the chat room.

--- chat/main.go
+++ chat/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
+	"github.com/google/uuid"
 )
 
 func main() {
+	// Create our server and configure the middleware that will help us to establish the WebSocket connection.
 	app := fiber.New()
-
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		// IsWebSocketUpgrade returns true if the client
 		// requested upgrade to the WebSocket protocol.
@@ -20,6 +22,13 @@ func main() {
 		return fiber.ErrUpgradeRequired
 	})
 
+	// Initiate the Pub/Sub service created by us.
+	pubsub := NewPubSub()
+	// In this case we only have one topic, one chat room.
+	subscribe := pubsub.Subscribe("chat")
+	// With this will save all the websocket connections that are active.
+	connections := make(map[string]*websocket.Conn)
+
 	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
 		// c.Locals is added to the *websocket.Conn
 		log.Println(c.Locals("allowed"))  // true
@@ -27,29 +36,63 @@ func main() {
 		log.Println(c.Query("v"))         // 1.0
 		log.Println(c.Cookies("session")) // ""
 
-		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+		// Create an Id to identify the connection.
+		id := uuid.New()
+		// Save the connection in our pool. And remove the connection when the websocket is close.
+		connections[id.String()] = c
+		defer delete(connections, id.String())
+
 		var (
 			mt  int
 			msg []byte
 			err error
 		)
 		for {
+			// We read everything we receive from the front-end.
 			if mt, msg, err = c.ReadMessage(); err != nil {
 				log.Println("read:", err)
 				break
 			}
-			log.Printf("recv: %s", msg)
+			log.Printf("recv: %s - mt: %d", msg, mt)
 
-			if err = c.WriteMessage(mt, msg); err != nil {
-				log.Println("write:", err)
-				break
+			// We get the data from the message and publish that in our Pub/Sub service.
+			m := Msg{}
+			err := json.Unmarshal(msg, &m)
+			if err != nil {
+				log.Println(err)
+				continue
 			}
+			m.Id = id.String() // add the Id to identify the owner
+			pubsub.Publish("chat", m)
 		}
 
 	}))
 
+	// Every time that receive a message, we will send the message to all the active connections.
+	// For that we need the channel for communication and all the connections.
+	go func(subs <-chan Msg, conn map[string]*websocket.Conn) {
+		for {
+			if subs == nil {
+				panic("We need our subscriber.")
+			}
+			message := <-subs
+			m, err := json.Marshal(message)
+			if err != nil {
+				log.Println("Error while marshal our message: ", err)
+				continue
+			}
+
+			for _, c := range conn {
+				if err = c.WriteMessage(1, m); err != nil {
+					log.Printf("Error while write to ID: %s with err: %s\n", message.Id, err)
+				}
+			}
+
+		}
+
+	}(subscribe, connections)
+
 	log.Fatal(app.Listen(":8080"))
 	// Access the websocket server: ws://localhost:3000/ws/123?v=1.0
 	// https://www.websocket.org/echo.html
-
 }
